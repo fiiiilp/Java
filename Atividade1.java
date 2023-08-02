00a8c3987ce7e5e9f38fd434f13e249d6ba234fe
fiiiilp <ffafwqerwrwrw@gmail.com>
2023-08-02 17:49:42 -0300

    Atividade Java

---
+++ Atividade1.java
@@ -0,0 +1,20 @@
+package principal;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Nome = "Filipe";
+		String Sobrenome = "Braz Rodrigues";
+		
+		//
+		
+		System.out.print("Meu nome é :" + Nome);
+		System.out.print(" " + Sobrenome);
+		
+		
+		
+		
+	}
+
+}
