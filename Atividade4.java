3f9cd4b84b3af4fda04454cc7f6d92abdd5431ae
fiiiilp <ffafwqerwrwrw@gmail.com>
2023-08-03 17:08:20 -0300

    Atividade 03/08

---
+++ Atividade4.java
@@ -0,0 +1,25 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Atividade4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+int numero;		
+
+System.out.print("Digite um número : ");
+numero = leia.nextInt();
+
+System.out.println("O número informado foi : " + numero);		
+		
+		
+		
+	// ATIVIDADE 03/08/2023 \\**	
+		
+		
+		leia.close();
+	}
+
+}
