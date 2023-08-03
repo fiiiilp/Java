5f6f59459d570f46bcdb923a89441532c25060b3
fiiiilp <ffafwqerwrwrw@gmail.com>
2023-08-03 15:28:24 -0300

    AtividadeComentarioJava

---
+++ AtividadeComentario.java
@@ -0,0 +1,31 @@
+package principal;
+
+public class AtividadeComentario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int numero1 = 30;
+		double numero2 = 20;
+		char opcao = 'S';
+		String mensagem = "Atividade1";
+		boolean valor = true;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(opcao);
+		System.out.println(mensagem);
+		System.out.println(valor);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
